17c58825219d35cc46c22726bdd9e80f42bdc879
Vinit <vinit.x.kumar@tsgforce.com>
2019-07-20 15:01:47 +0800

    ADadd

---
+++ src/main/resources/block.js
@@ -0,0 +1,41 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block {
+    constructor(timestamp, lastHash, hash, data) {
+        this.timestamp = timestamp;
+        this.lastHash = lastHash;
+        this.hash = hash;
+        this.data = data;
+    }
+
+    toString() {
+        return `Block -
+Timestamp : ${this.timestamp}
+Last Hash : ${this.lastHash.substring(0, 10)}
+Hash : ${this.hash.substring(0, 10)}
+Data : ${this.data}`;
+    }
+
+    static genesis() {
+        return new this('Genesis time', '----', 'genesis-hash', []);
+    }
+
+    static hash(timestamp, lastHash, data) {
+        return SHA256(`${timestamp}${lastHash}${data}`).toString();
+    }
+
+    static mineBlock(lastBlock, data) {
+        const timestamp = Date.now();
+        const lastHash = lastBlock.hash;
+        return new this(timestamp, lastHash, this.hash(timestamp,
+            lastHash, data), data);
+    }
+
+    static blockHash(block) {
+        //destructuring
+        const { timestamp, lastHash, data } = block;
+        return Block.hash(timestamp, lastHash, data);
+    }
+
+}
+module.exports = Block;
